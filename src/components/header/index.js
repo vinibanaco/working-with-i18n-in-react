9fbd0e028cd12044eb0e04ceba5474d9b2b0f4ce
Vinícius Marrara Banaco <vinibanaco@gmail.com>
2020-08-17 20:23:42 -0300

    feat: implement user context

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
 import styles from './index.module.css';
 import i18n from './i18n';
+import UserContext from '../../cross-cutting/contexts/user';
 
 function Header() {
+  const { language } = useContext(UserContext);
+
   return (
     <header className={styles.header}>
       <nav className={styles.menu}>
         <Link to="/page-1" className={styles.menuLink}>
-          {i18n.enUS.MENU_LINKS.PAGE_1}
+          {i18n[language].MENU_LINKS.PAGE_1}
         </Link>
         <Link to="/page-2" className={styles.menuLink}>
-          {i18n.enUS.MENU_LINKS.PAGE_2}
+          {i18n[language].MENU_LINKS.PAGE_2}
         </Link>
       </nav>
 
@@ -20,8 +23,8 @@ function Header() {
         <button className={styles.languageButton} disabled>
           <span
             role="img"
-            title={i18n.enUS.LANGUAGE_BUTTONS.PORTUGUESE}
-            aria-label={i18n.enUS.LANGUAGE_BUTTONS.PORTUGUESE}
+            title={i18n[language].LANGUAGE_BUTTONS.PORTUGUESE}
+            aria-label={i18n[language].LANGUAGE_BUTTONS.PORTUGUESE}
           >
             🇧🇷
           </span>
@@ -30,8 +33,8 @@ function Header() {
         <button className={styles.languageButton} disabled>
           <span
             role="img"
-            title={i18n.enUS.LANGUAGE_BUTTONS.ENGLISH}
-            aria-label={i18n.enUS.LANGUAGE_BUTTONS.ENGLISH}
+            title={i18n[language].LANGUAGE_BUTTONS.ENGLISH}
+            aria-label={i18n[language].LANGUAGE_BUTTONS.ENGLISH}
           >
             🇺🇸
           </span>
--- src/modules/page-1/index.js
+++ src/modules/page-1/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import styles from './index.module.css';
 import i18n from './i18n';
 
+import UserContext from '../../cross-cutting/contexts/user';
+
 function Page1() {
+  const { language } = useContext(UserContext);
+
   return (
     <>
-      <h1 className={styles.title}>{i18n.enUS.TITLE}</h1>
-      <p className={styles.text}>{i18n.enUS.TEXT}</p>
+      <h1 className={styles.title}>{i18n[language].TITLE}</h1>
+      <p className={styles.text}>{i18n[language].TEXT}</p>
     </>
   );
 }
